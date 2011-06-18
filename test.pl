0faf33a31492a5586ee840ffcaae2304644cf963
Tom Lanyon <tom@netspot.com.au>
2011-06-18 11:59:45 +0930

    updating test to play with optional cache_key.

--- test.pl
+++ test.pl
@@ -5,7 +5,13 @@ use Moose -traits => 'HasCache';
 
 has 'foo' => (
     traits => ['Cacheable'],
-    cache_key => 'foo',
+    isa => 'Str',
+    is  => 'rw',
+    lazy_build => 1,
+);
+
+has 'bar' => (
+    traits => ['Cacheable'],
     isa => 'Str',
     is  => 'rw',
     lazy_build => 1,
@@ -16,6 +22,13 @@ sub _build_foo {
     sleep 10;
     return join '', map { chr(int(rand(93))+33) } 1..40;
     warn "finished building foo";
+}
+
+sub _build_bar {
+    warn "building bar";
+    sleep 10;
+    return join '', map { chr(int(rand(93))+33) } 1..40;
+    warn "finished building bar";
 }
 
 
@@ -30,14 +43,19 @@ use Cache::FastMmap;
 
 Foo->meta->cache( Cache::FastMmap->new() );
 
-print "\n[", scalar localtime, "] getting foo once...\n";
+print "\n[", scalar localtime, "] getting foo1 without cache key...\n";
 my $foo1 = Foo->new;
 print $foo1->foo, "\n";
 
-print "\n[", scalar localtime, "] getting foo twice...\n";
+print "\n[", scalar localtime, "] getting bar1 with cache key...\n";
+$foo1->meta->find_attribute_by_name('bar')->cache_key( 'cached_bar' );
+print $foo1->bar, "\n";
+
+print "\n[", scalar localtime, "] getting foo2 with cache key...\n";
 my $foo2 = Foo->new;
+$foo2->meta->find_attribute_by_name('foo')->cache_key( 'cached_foo' );
 print $foo2->foo, "\n";
 
-print "\n[", scalar localtime, "]\n";
-
-
+print "\n[", scalar localtime, "] getting bar2 with cache key...\n";
+$foo2->meta->find_attribute_by_name('bar')->cache_key( 'cached_bar' );
+print $foo2->bar, "\n";
